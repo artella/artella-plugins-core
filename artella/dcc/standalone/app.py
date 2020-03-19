62c32f1e365b1d93d99a483a5ad31a64bebe79aa
Tomi Poveda <tpovedatd@gmail.com>
2020-03-19 01:12:43 +0100

    Added standalone version function implementation
    Standalone is used when the framework is used in a non DCC application.

---
+++ artella/dcc/standalone/app.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains Standalone application implementation
+"""
+
+from __future__ import print_function, division, absolute_import
+
+
+def version():
+    """
+    Returns version of the current DCC application
+
+    :return: Returns integer number indicating the version of the DCC application
+    :rtype: tuple(int)
+    """
+
+    return 0, 0, 0
