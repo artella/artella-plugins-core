7f11670d709da98efc120d291b35233445eaf107
Tomi Poveda <tpovedatd@gmail.com>
2020-04-01 00:49:33 +0200

    Added base modules for Artella Plugin DCC implementation

---
+++ artella/dcc/houdini/callback.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains Houdini DCC callback implementation
+"""
+
+from __future__ import print_function, division, absolute_import
+
+
+# TODO (tpoveda): Keeps this file empty for now until we implement callback functionality for 3ds Max. Do not remove
+# TODO (tpoveda): this module.
