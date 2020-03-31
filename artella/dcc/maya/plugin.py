1309904fd7e91001d153079f22dcf756449f9c02
Tomi Poveda <tpovedatd@gmail.com>
2020-04-01 00:47:21 +0200

    Added function implementation for setup project in Maya

--- artella/dcc/maya/plugin.py
+++ artella/dcc/maya/plugin.py
@@ -7,7 +7,8 @@ Module that contains Maya DCC plugin specific implementation
 
 from __future__ import print_function, division, absolute_import
 
-import maya.utils as utils
+import maya.cmds as cmds
+import maya.mel as mel
 
 import artella
 import artella.plugin as plugin
@@ -17,6 +18,24 @@ from . import utils as maya_utils
 
 
 class ArtellaMayaPlugin(plugin.ArtellaPlugin, object):
+
+    def setup_project(self, artella_local_root_path):
+        """
+        Setup Artella local root as current DCC active project
+        This function should be override in specific DCC plugin implementation
+        Is not an abstract function because its implementation is not mandatory
+
+        :param str artella_local_root_path: current user Artella local root path
+        """
+
+        artella_local_root_path = cmds.encodeString(artella_local_root_path)
+        mel.eval('setProject "%s"' % artella_local_root_path.replace('\\', '\\\\'))
+        cmds.workspace(directory=artella_local_root_path)
+        cmds.workspace(fileRule=['sourceImages', ''])
+        cmds.workspace(fileRule=['scene', ''])
+        cmds.workspace(fileRule=['mayaAscii', ''])
+        cmds.workspace(fileRule=['mayaBinary', ''])
+        artella.log_info('Set Maya Workspace Path: {}'.format(artella_local_root_path))
 
     def init(self):
         """
