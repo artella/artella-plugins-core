eeff93fff16ff661c5fd148d9594cbaf1a937e22
Tomi Poveda <tpovedatd@gmail.com>
2020-04-01 00:45:08 +0200

    Added new class in utils module to track new added files to a scene
    + Added TrackNodes class. This class can be instantiated to store the state of a Maya scene. After doing some operations in the scene you can call get_delta function to retrieve the nodes that have been removed/added to the scene after the operations.

--- artella/dcc/maya/utils.py
+++ artella/dcc/maya/utils.py
@@ -45,3 +45,50 @@ def get_maya_window():
     ptr = OpenMayaUI.MQtUtil.mainWindow()
 
     return qtutils.wrapinstance(ptr, QtWidgets.QMainWindow)
+
+
+class TrackNodes(object):
+    """
+    Helps track new nodes that get added to a scene after a function is called
+
+    track_nodes = TrackNodes()
+    track_nodes.load()
+    fn()
+    new_nodes = track_nodes.get_delta()
+    """
+
+    def __init__(self, full_path=False):
+        self._nodes = None
+        self._node_type = None
+        self._delta = None
+        self._full_path = full_path
+
+    def load(self, node_type=None):
+        """
+        Initializes TrackNodes states
+
+        :param str node_type: Maya node type we want to track. If not given, all current scene objects wil lbe tracked
+        """
+
+        self._node_type = node_type
+        if self._node_type:
+            self._nodes = cmds.ls(type=node_type, long=self._full_path)
+        else:
+            self._nodes = cmds.ls()
+
+    def get_delta(self):
+        """
+        Returns the new nodes in the Maya scene created after load() was executed
+
+        :return: List with all new nodes available in current DCC scene
+        :rtype: list(str)
+        """
+
+        if self._node_type:
+            current_nodes = cmds.ls(type=self._node_type, long=self._full_path)
+        else:
+            current_nodes = cmds.ls(long=self._full_path)
+
+        new_set = set(current_nodes).difference(self._nodes)
+
+        return list(new_set)
