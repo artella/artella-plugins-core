d2da746d35b3111664c64fc210a0a0bea8b380c6
Tomi Poveda <tpovedatd@gmail.com>
2020-04-01 22:51:13 +0200

    Removed specific DCC is... functions because they are not scalable

--- artella/dcc/__init__.py
+++ artella/dcc/__init__.py
@@ -27,56 +27,6 @@ class DccCallbacks(object):
     BeforeCreateReferenceCheck = ('BeforeCreateReferenceCheck', {'type': 'simpple'})
 
 
-def is_standalone():
-    """
-    Returns whether or not current application is standalone (no DCC)
-    :return: True if current session application is standalone; False otherwise
-    :rtype: bool
-    """
-
-    return artella.current_dcc() == 'standalone'
-
-
-def is_maya():
-    """
-    Returns whether or not current DCC is Maya
-    :return: True if current session DCC is Maya; False otherwise
-    :rtype: bool
-    """
-
-    return artella.current_dcc() == 'maya'
-
-
-def is_blender():
-    """
-    Returns whether or not current DCC is Blender
-    :return: True if current session DCC is Blender; False otherwise
-    :rtype: bool
-    """
-
-    return artella.current_dcc() == 'blender'
-
-
-def is_max():
-    """
-    Returns whether or not current DCC is 3ds Max
-    :return: True if current session DCC is 3ds Max; False otherwise
-    :rtype: bool
-    """
-
-    return artella.current_dcc() == 'max'
-
-
-def is_houdini():
-    """
-    Returns whether or not current DCC is Houdini
-    :return: True if current session DCC is Houdini; False otherwise
-    :rtype: bool
-    """
-
-    return artella.current_dcc() == 'houdini'
-
-
 def callbacks():
     """
     Returns a list of callbacks based on DCC available callbacks
