414cbd7d2207f206385279cfb2c91d36584f5107
Tomas Poveda <tpovedatd@gmail.com>
2020-05-06 20:24:53 +0200

    Added plugin and ui standalone modules implementation.

---
+++ artella/dcc/standalone/plugin.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains Standalone plugin specific implementation
+"""
+
+from __future__ import print_function, division, absolute_import
+
+from artella import register
+from artella.core import dccplugin
+from artella.core.utils import Singleton
+
+
+class ArtellaStandalonePlugin(dccplugin.ArtellaDccPlugin, object):
+
+    def init(self):
+        super(ArtellaStandalonePlugin, self).init()
+
+
+@Singleton
+class ArtellaStandalonePluginSingleton(ArtellaStandalonePlugin, object):
+    def __init__(self, artella_drive_client=None):
+        ArtellaStandalonePlugin.__init__(self, artella_drive_client=artella_drive_client)
+
+
+register.register_class('DccPlugin', ArtellaStandalonePluginSingleton)
