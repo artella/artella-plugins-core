cb94d84b33e5d13cde81f2467cb7167f3886d345
Tomas Poveda <tpovedatd@gmail.com>
2020-05-06 20:14:21 +0200

    Added new functions to app, callback and ui abstract modules

--- artella/dcc/abstract/ui.py
+++ artella/dcc/abstract/ui.py
@@ -7,7 +7,7 @@ Module that contains DCC abstract UI implementation
 
 from __future__ import print_function, division, absolute_import
 
-from artella import reroute
+from artella.core.dcc import reroute
 from artella.core.utils import abstract
 
 
