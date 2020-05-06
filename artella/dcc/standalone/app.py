88e329451f659f2f197f67e0ba66ef25ba4856a5
Tomas Poveda <tpovedatd@gmail.com>
2020-05-06 20:25:09 +0200

    Implemented new functions in App Standalone module

--- artella/dcc/standalone/app.py
+++ artella/dcc/standalone/app.py
@@ -7,6 +7,19 @@ Module that contains Standalone application implementation
 
 from __future__ import print_function, division, absolute_import
 
+from artella.core import utils
+
+
+def name():
+    """
+    Returns name of current DCC
+
+    :return: Returns name of DCC without any info about version
+    :rtype: str
+    """
+
+    return 'standalone'
+
 
 def version():
     """
@@ -17,3 +30,113 @@ def version():
     """
 
     return 0, 0, 0
+
+
+def extensions():
+    """
+    Returns a list of available extension for DCC application
+
+    :return: List of available extensions with the following format: .{EXTENSION}
+    :rtype: list(str)
+    """
+
+    return ['.ma', '*.mb', 'readme.txt']
+
+
+def scene_name():
+    """
+    Returns the name of the current scene
+
+    :return: Full file path name of the current scene. If no file is opened, None is returned.
+    :rtype: str or None
+    """
+
+    return ''
+
+
+def scene_is_modified():
+    """
+    Returns whether or not current opened DCC file has been modified by the user or not
+
+    :return: True if current DCC file has been modified by the user; False otherwise
+    :rtype: bool
+    """
+
+    return False
+
+
+def open_scene(file_path, save=True):
+    """
+    Opens DCC scene file
+    :param str file_path: Absolute local file path we want to open in current DCC
+    :param bool save: Whether or not save current opened DCC scene file
+    :return: True if the save operation was successful; False otherwise
+    :rtype: bool
+    """
+
+    pass
+
+
+def save_scene(force=True, **kwargs):
+    """
+    Saves DCC scene file
+
+    :param bool force: Whether to force the saving operation or not
+    :return:
+    """
+
+    pass
+
+
+def import_scene(file_path):
+    """
+    Opens scene file into current opened DCC scene file
+    :param str file_path: Absolute local file path we want to import into current DCC
+    :return: True if the import operation was successful; False otherwise
+    :rtype: bool
+    """
+
+    pass
+
+
+def reference_scene(file_path, **kwargs):
+    """
+    References scene file into current opened DCC scene file
+    :param str file_path: Absolute local file path we want to reference into current DCC
+    :return: True if the reference operation was successful; False otherwise
+    :rtype: bool
+    """
+
+    pass
+
+
+def supports_uri_scheme():
+    """
+    Returns whether or not current DCC support URI scheme implementation
+
+    :return: True if current DCC supports URI scheme implementation; False otherwise
+    """
+
+    return True
+
+
+def pass_message_to_main_thread(fn, data):
+    """
+    Executes given callable object in the DCC thread in the next idle event of that thread.
+    :param fn: callable object to execute
+    :param data: arguments to pass to the callable object
+    """
+
+    return fn(data)
+
+
+def clean_path(file_path):
+    """
+    Cleans given path so it can be properly used by current DCC
+
+    :param str file_path: file path we want to clean
+    :return: Cleaned version of the given file path
+    :rtype: str
+    """
+
+    return utils.clean_path(file_path)
