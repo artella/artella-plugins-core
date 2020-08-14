a7155855969109b99bdeeb4389f8c7ae9cdedc71
Tomas Poveda <tpovedatd@gmail.com>
2020-08-14 18:44:10 +0200

    Removed _reload function and added cleanup modules in init function

--- artella/loader.py
+++ artella/loader.py
@@ -69,6 +69,7 @@ def init(
     # Once DCC paths are registered we can import modules
     import artella
     import artella.register as register
+
     import artella.dcc as dcc
 
     # Do not remove this imports, they do registration of some of the core Artella classes
@@ -111,10 +112,11 @@ def init(
     return True
 
 
-def shutdown(dev=False):
+def shutdown(dev=False, cleanup_modules=False):
     """
     Shutdown Artella Plugin
 
+    :param bool cleanup_modules: Whether import artella modules should be removed from sys.path or not
     :return: True if Artella shutdown was successful; False otherwise.
     :rtype: bool
     """
@@ -130,26 +132,14 @@ def shutdown(dev=False):
 
     register.cleanup()
 
-    return True
-
-
-def _reload(dev=False):
-    """
-    Function to be used during development. Can be used to "reload" Artella modules.
-    Useful when working inside DCC envs.
-    """
-
-    # Create logger
-    logger.create_logger()
-
-    # We make sure that plugin is shutdown before doing reload
-    shutdown(dev=dev)
-
     # Cleanup artella modules
-    modules_to_reload = 'artella.'
-    for k in sys.modules.keys():
-        if k.startswith(modules_to_reload):
-            del sys.modules[k]
+    if cleanup_modules:
+        modules_to_reload = 'artella.'
+        for k in sys.modules.keys():
+            if k.startswith(modules_to_reload):
+                del sys.modules[k]
 
-    global CURRENT_DCC
-    CURRENT_DCC = None
+        global CURRENT_DCC
+        CURRENT_DCC = None
+
+    return True
